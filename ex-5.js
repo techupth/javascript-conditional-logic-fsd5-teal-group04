dd07ecf923603f5ad96a8430bcf8d7edca76ea60
Pao <chaiwatt77@gmail.com>
2023-11-30 13:52:06 +0700

    fix:ex1-7

--- ex-5.js
+++ ex-5.js
@@ -1,5 +1,18 @@
 function calculateStudentGrade(score) {
   // Start coding here
+  if(score < 60){
+    return "F";
+  } else {
+    if(score >= 60 && score <= 69){
+      return "D";
+    }else if(score >= 70 && score <= 79){
+      return "C";
+    }else if(score >= 80 && score <= 89){
+      return "B";
+    }else if(score >= 90 && score <= 100){
+      return "A";
+    } 
+  }
 }
 
 // Example case
